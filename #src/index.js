d79d858ec6757f8c990efa0bd792e9975e2f5502
Maxim A <m_altuhov@mail.ru>
2021-02-23 20:41:32 +0300

    Update for Webpack 5 and add/config eslint

--- #src/index.js
+++ #src/index.js
@@ -1,14 +1,11 @@
-'use strict';
-
-// import $ from 'jquery';
-// window.jQuery = $;
-// window.$ = $;
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 
 import {
   testLog
 } from '@page/test1/test.js';
 import './index.css';
-import './less.less';
 import './scss.scss';
 
 function contentLoaded() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,119 +1,67 @@
-'use strict';
-
 const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
-const {
-  CleanWebpackPlugin
-} = require('clean-webpack-plugin');
-const {
-  ALL
-} = require('dns');
-
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
-
-const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
-
+const filename = ext => isDev ? `[name].${ext}` : `[name].[fullhash].${ext}`;
 const cssLoaders = add => {
-  const loaders = [{
+  const loaders = [
+    {
       loader: MiniCssExtractPlugin.loader,
-      options: {},
+      options: {
+        publicPath: (resourcePath, context) => {
+          return path.relative(path.dirname(resourcePath), context) + '/';
+        }
+      }
     },
     'css-loader',
+    {
+      loader: 'postcss-loader',
+      options: {
+        postcssOptions: {
+          plugins: [
+            [
+              'postcss-preset-env',
+              {
+                browsers: 'last 3 versions',
+                autoprefixer: {
+                  grid: true
+                }
+              }
+            ]
+          ]
+        }
+      }
+    }
   ];
-
   if (add) {
     loaders.push(add);
   }
-
   return loaders;
 };
-
 const babelOptions = presets => {
   const option = {
     presets: [
-      '@babel/preset-env'
+      ['@babel/preset-env', {
+        useBuiltIns: 'usage',
+        corejs: 3
+      }]
     ],
-    plugins: [
-      '@babel/plugin-proposal-class-properties'
-    ]
+    plugins: ['@babel/plugin-proposal-class-properties']
   };
-
   if (presets) {
     option.presets.push(presets);
   }
-
   return option;
 };
-
-const jsLoaders = () => {
-  const loaders = [{
-    loader: 'babel-loader',
-    options: babelOptions()
-  }];
-
-  if (isDev) {
-    loaders.push('eslint-loader');
-  }
-
-  return loaders;
-};
-
-//оптимизация финального бандла
-const optimization = () => {
-  const config = {
-    // общие данные выводятся в отдельный файл
-    // splitChunks: {
-    //   chunks: 'all'
-    // }
-  };
-
-  if (isProd) {
-    config.minimizer = [
-      new OptimizeCssAssetsWebpackPlugin(),
-      new TerserWebpackPlugin()
-    ];
-  }
-  return config;
-};
-
-module.exports = {
-  context: path.resolve(__dirname, '#src'),
-  mode: 'development',
-  entry: {
-    main: ['@babel/polyfill', './index.js'],
-    // add: ['@babel/polyfill','./base/js/add.js']
-  },
-  output: {
-    filename: filename('js'),
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    // extensions: ['.js'] // форматы файлов
-    alias: {
-      '@base': path.resolve(__dirname, '#src/base'),
-      '@page': path.resolve(__dirname, '#src/pages'),
-      '@comp': path.resolve(__dirname, '#src/components'),
-      '@': path.resolve(__dirname, '#src')
-    }
-  },
-  optimization: optimization(),
-  devServer: {
-    port: 4200,
-    open: true
-  },
-  devtool: isDev === true ? 'source-map' : false,
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery'
-    }),
+const plugins = () => {
+  const base = [
     new HTMLWebpackPlugin({
       template: './index.html',
       minify: {
@@ -130,57 +78,106 @@ module.exports = {
       }]
     }),
     new MiniCssExtractPlugin({
-      filename: filename('css')
+      filename: `css/${filename('css')}`
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
     })
-  ],
+  ];
+  if (isDev) {
+    base.push(new ESLintPlugin());
+  }
+  return base;
+};
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  };
+  if (isProd) {
+    config.minimize = true;
+    config.minimizer = [new CssMinimizerPlugin(), new TerserWebpackPlugin()];
+  }
+  return config;
+};
+module.exports = {
+  context: path.resolve(__dirname, '#src'),
+  mode: 'development',
+  entry: {
+    main: ['./index.js']
+    // add: ['./base/js/add.js']
+  },
+  output: {
+    filename: `js/${filename('js')}`,
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    // extensions: ['.js', '.json', '.html'], // форматы файлов
+    alias: {
+      '@base': path.resolve(__dirname, '#src/base'),
+      '@fonts': path.resolve(__dirname, '#src/base/assets/fonts'),
+      '@img': path.resolve(__dirname, '#src/base/assets/img'),
+      '@page': path.resolve(__dirname, '#src/pages'),
+      '@comp': path.resolve(__dirname, '#src/components')
+    }
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    open: true
+  },
+  target: isDev === true ? 'web' : 'browserslist',
+  devtool: isDev === true ? 'source-map' : false,
+  plugins: plugins(),
   module: {
-    rules: [{
-        test: /\.css$/,
-        use: cssLoaders()
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
       },
       {
-        test: /\.less$/,
-        use: cssLoaders('less-loader')
+        test: /\.css$/,
+        use: cssLoaders()
       },
       {
         test: /\.(sass|scss)$/,
         use: cssLoaders('sass-loader')
       },
       {
-        test: /\.(png|jpg|svg|gif|webp}$)/,
-        use: ['file-loader']
+        test: /\.(?:ico|png|jpg|jpeg|svg|gif|webp)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: '@img',
+          name() {
+            if (isDev) {
+              return '[name].[ext]';
+            }
+            return '[name].[fullhash].[ext]';
+          }
+        }
       },
       {
-        test: /\.(ttf|woff|woff2|eot}$)/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader']
-      },
-      {
-        test: /\.csv$/,
-        use: ['csv-loader']
+        test: /\.(ttf|woff|woff2|eot|otf)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: '@fonts',
+          name() {
+            if (isDev) {
+              return '[name].[ext]';
+            }
+            return '[name].[fullhash].[ext]';
+          }
+        }
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: jsLoaders()
-      },
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: babelOptions('@babel/preset-typescript')
-        }
-      },
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: babelOptions('@babel/preset-react')
+          options: babelOptions()
         }
       }
     ]
