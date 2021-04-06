3f98becf62e20fceb804663580792db000f9a3a9
Maxim A <m_altuhov@mail.ru>
2021-04-06 13:56:47 +0300

    Правка работы слайдера и добавил переменную для градиента

--- src/components/card-room/card-room.js
+++ src/components/card-room/card-room.js
@@ -4,16 +4,29 @@ SwiperCore.use([Navigation, Pagination]);
 
 // слайдер
 function sliderswiper() {
-  const swiper = new SwiperCore('.js-card-room__images', {
-    loop: true,
-    watchOverflow: true,
-    pagination: {
-      el: '.js-card-room__pagination',
-      clickable: true
-    },
-    navigation: {
-      nextEl: '.js-card-room__btn_next',
-      prevEl: '.js-card-room__btn_prev'
+  const sliders = document.querySelectorAll('.js-card-room__images');
+
+  sliders.forEach(elem => {
+    const swiper = new SwiperCore(elem, {
+      loop: true,
+      watchOverflow: true,
+      pagination: {
+        el: '.js-card-room__pagination',
+        clickable: true
+      },
+      navigation: {
+        nextEl: elem.querySelector('.js-card-room__btn_next'),
+        prevEl: elem.querySelector('.js-card-room__btn_prev')
+      }
+    });
+
+    if (swiper.imagesToLoad.length === 3 || swiper.imagesToLoad.length === 1) {
+      const navArrows = elem.querySelectorAll('.js-card-room__btn');
+
+      navArrows.forEach(item => {
+        item.remove();
+      });
+      swiper.destroy();
     }
   });
 }
