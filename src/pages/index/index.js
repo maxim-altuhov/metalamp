8663b6f002ac71e83df0bdefa9a6e423f2ccd5c0
Maxim A <m_altuhov@mail.ru>
2021-04-13 17:27:24 +0300

    рефакторинг кода компонентов и проставил теги заголовков

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,16 @@
 import './index.scss';
 
-function contentLoaded() {}
+import { checkedFocus } from '@base/base';
+import initDropdown from '@comp/dropdown/dropdown';
+import addDatePicker from '@comp/input/input-datepicker';
+
+function contentLoaded() {
+  checkedFocus();
+  initDropdown({});
+  addDatePicker({
+    $selectorId: '#card-search-date',
+    secondSelector: true
+  });
+}
 
 window.addEventListener('DOMContentLoaded', contentLoaded);
---
+++ src/pages/search-room/search-room.js
@@ -0,0 +1,9 @@
+import './search-room.scss';
+
+import { checkedFocus } from '@base/base';
+
+function contentLoaded() {
+  checkedFocus();
+}
+
+window.addEventListener('DOMContentLoaded', contentLoaded);
